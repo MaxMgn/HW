80562e4c29b1a067ed7a7a6b2c6396ef10fdb5b0
MaxMgn <m17111987@gmail.ru>
2022-08-25 21:50:50 +0500

    HW1

---
+++ HW1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (number1 > number2 )
+{
+   max = number1;
+   min = number2;
+   Console.Write("Большее число = " + max + "," + "Меньшее число = " + min);
+}
+else if  (number1 < number2 )
+{
+   max = number2;
+   min = number1;
+   Console.Write("Большее число = " + max + "," + "Меньшее число = " + min);
+}
+else
+{
+    Console.Write("Числа равны");
+}
---
+++ HW1_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+string result = "";
+while (index <= number)
+{
+    if (index % 2 == 0 && index != 2)
+    {
+        result = result + ", " + index; 
+    }
+    else if (index == 2)
+    {
+        result = result + index; 
+    }
+    index++;
+}
+
+Console.Write(result);
+
+
